f67c4bc1adaa9410ab8ee85db6c50bbac545a924
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2021-10-19 13:25:16 -0700

    Update script.js

--- JavaScript/FrenchCards/script.js
+++ JavaScript/FrenchCards/script.js
@@ -1,43 +1,130 @@
-// define the object for the question entity
-let question = {
-  title: 'chat',
-  alternatives: ['cat', 'dog', 'fish', 'frog'],
-  correctAnswer: 1
+let questions = [
+    {
+        title: 'chien',
+        alternatives: ['dog', 'cat', 'bird', 'fish'],
+        correctAnswer: 0
+    },
+    {
+        title: 'oiseau',
+        alternatives: ['hamster', 'mouse', 'lizard', 'bird'],
+        correctAnswer: 3
+    },
+    {
+        title: 'chat',
+        alternatives: ['rat', 'fish', 'shark', 'cat'],
+        correctAnswer: 3
+    },
+    {
+        title: 'mouche',
+        alternatives: ['fly', 'puma', 'fish', 'dog'],
+        correctAnswer: 0
+    }
+];
+
+
+let app = {
+    start: function() {
+
+        this.currPosition = 0;
+        this.score = 0;
+
+        // get alternatives
+        let alts = document.querySelectorAll('.alternative');
+
+        alts.forEach((element, index) => {
+
+            element.addEventListener('click', () => {
+                // check correct answer
+                this.checkAnswer(index);
+            });
+        });
+
+        // refresh stats
+        this.updateStats();
+
+        // show first question
+        this.showQuestion(questions[this.currPosition]);
+    },
+
+    showQuestion: function(q) {
+
+        // show question title
+        let titleDiv = document.getElementById('title');
+        titleDiv.textContent = q.title;
+
+        // show alternatives
+        let alts = document.querySelectorAll('.alternative');
+
+        alts.forEach(function(element, index){
+            element.textContent = q.alternatives[index];
+        });
+    },
+
+    checkAnswer: function(userSelected) {
+
+        let currQuestion = questions[this.currPosition];
+
+        if(currQuestion.correctAnswer ==  userSelected) {
+            // correct
+            console.log('correct');
+            this.score++;
+            this.showResult(true);
+        }
+        else {
+            // not correct
+            console.log('wrong');
+            this.showResult(false);
+        }
+
+        // refresh stats
+        this.updateStats();
+
+        // increase position
+        this.increasePosition();
+
+        // show next question
+        this.showQuestion(questions[this.currPosition]);
+    },
+
+    increasePosition: function() {
+        this.currPosition++;
+
+        if(this.currPosition == questions.length) {
+            this.currPosition = 0;
+        }
+    },
+
+    updateStats: function() {
+        let scoreDiv = document.getElementById('score');
+        scoreDiv.textContent = `Your score: ${this.score}`;
+    },
+
+    showResult: function(isCorrect) {
+        let resultDiv = document.getElementById('result');
+        let result = '';
+
+        // checks
+        if(isCorrect) {
+            result = 'Correct Answer!';
+        }
+        else {
+            // get the current question
+            let currQuestion = questions[this.currPosition];
+
+            // get correct answer (index)
+            let correctAnswIndex = currQuestion.correctAnswer;
+
+            // get correct answer (text)
+            let correctAnswText = currQuestion.alternatives[correctAnswIndex];
+
+            result = `Wrong! Correct answer: ${correctAnswText}`;
+        }
+
+        resultDiv.textContent = result;
+    }
+
+
+
 };
 
-// function for showing the question
-function showQuestion(q) {
-  // show question title
-  let titleDiv = document.getElementById('title');
-  titleDiv.textContent = q.title;
-  
-  // show alternatives
-  let alts = document.querySelectorAll('.alternative');
-  
-  alts.forEach(function(element, index){
-    element.textContent = q.alternatives[index];
-    
-    // listening for events
-    element.addEventListener('click', function(){
-      // check correct answer
-      if(q.correctAnswer == index) {
-        console.log('Correct Answer!');
-      }
-      else {
-        console.log('Wrong Answer!');
-      }
-    });
-    
-  });
-}
-
-// call the function
-showQuestion(question);
-
-// select the button element
-let btn = document.getElementById('btn');
-
-// listening to click events
-btn.addEventListener('click', function(){
-  console.log('Clicked!');
-});
+app.start();
